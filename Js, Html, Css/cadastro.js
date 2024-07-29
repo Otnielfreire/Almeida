af615db8d61e1e0c22dd8c41c1cddf8276dd3539
Otnielfreire <otnielaf@gmail.com>
2024-07-29 13:39:41 -0300

    Add files via upload

---
+++ Js, Html, Css/cadastro.js
@@ -0,0 +1,52 @@
+const cadastroForm = document.getElementById('cadastroForm');
+const mensagem = document.getElementById('mensagem');
+const listaAlunos = document.getElementById('listaAlunos');
+
+let alunos = [];
+
+cadastroForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const telefone = document.getElementById('telefone').value;
+
+    if (nome && telefone) {
+        alunos.push({ nome, telefone });
+
+        atualizarListaAlunos();
+
+        mensagem.textContent = `Aluno cadastrado com sucesso!`;
+        mensagem.style.color = '#28a745';
+
+        cadastroForm.reset();
+    } else {
+        mensagem.textContent = 'Por favor, preencha todos os campos.';
+        mensagem.style.color = '#d9534f';
+    }
+});
+
+function atualizarListaAlunos() {
+    listaAlunos.innerHTML = '';
+    alunos.forEach(aluno => {
+        const li = document.createElement('ol'); 
+        li.textContent = `Nome: ${aluno.nome}, Telefone: ${aluno.telefone}`; 
+        listaAlunos.appendChild(li);
+    })
+}
+// Função para formatar o telefone (DDD) X XXXX-XXXX enquanto o usuário digita
+document.getElementById('telefone').addEventListener('input', function (event) {
+    var telefone = event.target.value;
+
+    // Remove tudo que não é dígito
+    telefone = telefone.replace(/\D/g, '');
+
+    // Formatação conforme o número de dígitos
+    if (telefone.length === 11) {
+        telefone = telefone.replace(/^(\d{2})(\d{1})(\d{4})(\d{4})/, '($1) $2 $3-$4');
+    } else if (telefone.length === 10) {
+        telefone = telefone.replace(/^(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
+    }
+
+    // Atualiza o valor no campo de telefone
+    event.target.value = telefone;
+});
