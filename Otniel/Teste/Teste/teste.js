8f4054aa4c988756dd5d3897150263adb93b9b69
Otnielfreire <otnielaf@gmail.com>
2024-08-09 09:44:38 -0300

    Add files via upload

---
+++ Otniel/Teste/Teste/teste.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const playButton = document.getElementById('playButton');
+    
+    playButton.addEventListener('click', () => {
+        alert('Play button clicked! This is where the video would start.');
+    });
+});
